a875c0f3188d5f9bca15356a9e15bd40c497c971
srinivasa29 <srinivasamannepula7@gmail.com>
2026-07-30 12:43:33 +0530

    First day of programming

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,10 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
-       //Write your code here
+        System.out.println("Welcome to the java track");
+        System.out.println("I already know how to build programming logic");
+        System.out.println("Now i am here learning how to express that logic in java");
+        System.out.println("My name is srinivasa");
+        System.out.println("My goal is to become a java developer");
+        System.out.println("I selected java because i want to build large applications");
     }
 }
