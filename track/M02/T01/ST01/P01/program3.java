4553c1b151cd6e3d19d4909d3cc676c0eb5e5ec7
srinivasa29 <srinivasamannepula7@gmail.com>
2026-08-03 15:03:02 +0530

    Pogram 3,4,5

---
+++ track/M02/T01/ST01/P01/program3.java
@@ -0,0 +1,9 @@
+public class program3{
+    public static void main(String[]args){
+        String learnerName="salman";
+        int dailyPracticeHours=8;
+        System.out.println("Name: " + learnerName);
+        System.out.println("Daily Practice: " +dailyPracticeHours + " hours");
+
+    }
+}
---
+++ track/M02/T01/ST01/P01/program4.java
@@ -0,0 +1,33 @@
+public class program4 {
+    public static void main(String[] args) {
+
+        // Write your code here
+
+        double principal = 10000.0;
+        double rate = 6.5;
+        double time = 2.0;
+        double weight = 72.0;
+        double height = 1.8;
+
+        int totalMarks = 78 + 84 + 69 + 91 + 88;
+
+        // Simple Interest
+        double si = principal * rate * time / 100.0;
+
+        // Total Amount
+        double totalAmount = principal + si;
+
+        // BMI
+        double bmi = weight / (height * height);
+
+        // Percentage
+        double percentage = totalMarks * 100.0 / 500;
+
+        // Output
+        System.out.println("Simple Interest: " + si);
+        System.out.println("Total Amount: " + totalAmount);
+        System.out.println("BMI: " + bmi);
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Percentage: " + percentage);
+    }
+}
