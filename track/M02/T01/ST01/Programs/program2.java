6fd4456dfc5afd8fa3bbb6e6f8fe3288b4543bfc
srinivasa29 <srinivasamannepula7@gmail.com>
2026-08-04 12:29:07 +0530

    Day 3 Java Practice

--- track/M02/T01/ST01/Programs/program2.java
+++ track/M02/T01/ST01/Programs/program2.java
@@ -1,13 +1,15 @@
-public class program2 { 
-       public static void main(String[] args) {   
-             int javaHoursPerDay=2; 
-             int aptitudeHoursPerDay=1;
-             int numberOfDays=5;        
-             int weeklyJavaHours=javaHoursPerDay*numberOfDays;        
-             int weeklyAptitudeHours=aptitudeHoursPerDay*numberOfDays;        
-             int totalPreparationHours=weeklyJavaHours + weeklyAptitudeHours;        
-             System.out.println("Java: " + weeklyJavaHours);        
-             System.out.println("Aptitude: " + weeklyAptitudeHours);        
-             System.out.println("Total: " + totalPreparationHours); 
-            }
-        }
+
+public class program2 {
+
+    public static void main(String[] args) {
+        int javaHoursPerDay = 2;
+        int aptitudeHoursPerDay = 1;
+        int numberOfDays = 5;
+        int weeklyJavaHours = javaHoursPerDay * numberOfDays;
+        int weeklyAptitudeHours = aptitudeHoursPerDay * numberOfDays;
+        int totalPreparationHours = weeklyJavaHours + weeklyAptitudeHours;
+        System.out.println("Java: " + weeklyJavaHours);
+        System.out.println("Aptitude: " + weeklyAptitudeHours);
+        System.out.println("Total: " + totalPreparationHours);
+    }
+}
