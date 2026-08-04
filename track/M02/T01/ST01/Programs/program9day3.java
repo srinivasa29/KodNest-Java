076ab5b4342ad7e6af1f9541471a0a2905766e22
srinivasa29 <srinivasamannepula7@gmail.com>
2026-08-04 13:20:53 +0530

    Day 3 Java Practice

---
+++ track/M02/T01/ST01/Programs/program9day3.java
@@ -0,0 +1,17 @@
+
+public class program9day3 {
+
+    public static void main(String[] args) {
+
+        int marks = 72;
+        int attendance = 80;
+        boolean projectCompleted = true;
+
+        boolean marksEligible = marks >= 60;
+        boolean attendanceEligible = attendance >= 75;
+        boolean academicEligible = marksEligible && attendanceEligible;
+        boolean placementEligible = academicEligible && projectCompleted;
+
+        System.out.println("Placement Eligible: " + placementEligible);
+    }
+}
