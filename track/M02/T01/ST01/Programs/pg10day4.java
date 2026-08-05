cda034d66389dbc972d8eabf3045e8b97ec73ed7
srinivasa29 <srinivasamannepula7@gmail.com>
2026-08-05 11:20:19 +0530

    Day4

---
+++ track/M02/T01/ST01/Programs/pg10day4.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+
+public class pg10day4 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        int num = sc.nextInt();
+        switch (num) {
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            default:
+                System.out.println("Invalid Number");
+
+        }
+
+    }
+}
