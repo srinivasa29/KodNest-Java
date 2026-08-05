5f6b351f7dbba5dd08766dd4bf1341cfdeee8425
srinivasa29 <srinivasamannepula7@gmail.com>
2026-08-05 12:33:37 +0530

    DAY4 while

---
+++ track/M02/T01/ST01/Programs/pg13.java
@@ -0,0 +1,13 @@
+
+public class pg13 {
+
+    public static void main(String[] args) {
+        int i = 5;
+        while (i >= 1 )
+         {
+            System.out.println(i);
+            i--;
+        }    
+        
+    }
+}
